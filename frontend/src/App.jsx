79fec3221b7ecc4afdeb1a83964263f0b931263b
Sandesh Gnawali <sandeshgyawali25@gmail.com>
2023-12-06 02:16:46 -0600

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <h1>Sandesh Gnawali</h1>
+      <div className="card">
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
